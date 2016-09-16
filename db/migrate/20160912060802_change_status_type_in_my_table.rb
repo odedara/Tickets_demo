7a2fdd2ce8d0c7b971223d94fc5d196c14897d9e
manisha <comp.manisha7510@gmail.com>
2016-09-16 12:15:11 +0530

    changes in gem file and db migration

--- db/migrate/20160912060802_change_status_type_in_my_table.rb
+++ db/migrate/20160912060802_change_status_type_in_my_table.rb
@@ -1,5 +1,5 @@
 class ChangeStatusTypeInMyTable < ActiveRecord::Migration
   def change
-  	change_column :tickets, :status, :boolean
+  	#change_column :tickets, :status, :boolean, :default => false
   end
 end
